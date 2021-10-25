a534f3d782f20dee6ae978318363f127e5be00d5
Matt Harrop <osaintspreserveus@live.com>
2021-10-25 11:24:09 -0700

    Added basic EC2 (boto3) access to the user homepage.

--- game_server_management/views.py
+++ game_server_management/views.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render
-
+import boto3
 
 # Create your views here.
 
 @login_required
 def home(request):
-    return render(request, 'game_server_management/index.html')
+    ec2 = boto3.client('ec2')
+    response = ec2.describe_instances()
+    instanceName = response['Reservations'][0]['Instances'][0]['KeyName']
+
+    return render(request, 'game_server_management/index.html', {'instanceName': instanceName})
