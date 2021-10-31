ed309b8afbaa708d94887f18555e25b4d5135c4f
Matt Harrop <osaintspreserveus@live.com>
2021-10-31 09:12:54 -0700

    Fixing typo in deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo git pull origin master
-sudo pip3 install -r requirements.txt
+sudo pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic
